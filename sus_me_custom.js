2688a625f8ce3d84c8f97f3278d8233c3fcbf1f8
Dean Corbin <DCorbin42@gmail.com>
2026-07-01 13:51:03 -0400

    Update sus_me_custom.js

--- sus_me_custom.js
+++ sus_me_custom.js
@@ -132,7 +132,7 @@ function click2Open(event){
 		'winTitle': winTitle
 	});
 	
-	openRadWindowEx(winTitle, url, properties, () => console.log(window)); //testing
+	openRadWindowEx(winTitle, url, properties, () => Form.Refresh(), null); //testing
 }
 
 
